3af9102ef3295ec978c119318ff40c89817899a4
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-01-30 21:02:25 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+
+	switch (action.type) {
+		case 'ADD_FRIEND': return {friends: [...state.friends, action.friend]}
+		case 'REMOVE_FRIEND':
+			let newArray = state.friends.filter( friend => {
+				if (friend.id !== action.id) {
+					return friend
+				}
+			})
+			return {friends: newArray}
+		default: return state
+	}
+
 }
