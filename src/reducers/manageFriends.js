634e074723d045729d1ea6c105d4ddb2b79fb4f0
Meg Galvez <meg@oakmaine.com>
2020-02-01 10:49:40 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {...state, friends:[...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            const newState = {...state} 
+            newState.friends = state.friends.filter(f => f.id !== action.id)
+            return newState
+        default:
+            return state 
+
+    }
+
+
 }
