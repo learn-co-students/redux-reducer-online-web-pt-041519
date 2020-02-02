74e968a99ffa745f7044ea13cc91ffd393362240
imaglazer <avigayilglazer@gmail.com>
2020-02-02 20:47:11 +0200

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    // state = {
+    //     friends: []
+    // }
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return (
+                // console.log(action.type),
+                {...state, 
+                    friends: [
+                        ...state.friends,
+                        action.friend
+              ]
+            }
+         )
+        
+        case 'REMOVE_FRIEND':
+          return (
+              {friends:
+                state.friends.filter(friend => friend.id !== action.id)
+            }
+              
+          )
+
+        default:
+          return state;
+      }
 }
