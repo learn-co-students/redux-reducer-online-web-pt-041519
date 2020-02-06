2a38a570a32242b87b80e0fec829a61cdd9939a8
Renee Nordholm <reneenordholm@gmail.com>
2020-02-06 17:03:39 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
+let state = {friends: []}
+
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {friends: [ 
+              { name: 'Avi', 
+                hometown: 'NYC', 
+                id: 100 }, 
+              { name: 'Joe', 
+                hometown: 'Boston', 
+                id: 101 } 
+            ]
+          }
+        // case 'REMOVE_FRIEND':
+        //   return {friends: state.id - 1}
+        default:
+          return state;
+    }
 }
